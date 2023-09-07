1d351d3640ad239527d68cd2f3fbb77c3c7bec33
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-07 12:24:22 -0300

    refactor: aproxima variaveis para perto de sua utilização

--- backend/ride/src/CpfValidator.ts
+++ backend/ride/src/CpfValidator.ts
@@ -6,26 +6,21 @@ export default class CpfValidator{
     if (cpf.length !== 11) return false
     if (cpf.split("").every(c => c === cpf[0])) return false
     let d1 = 0
-    let d2 = 0;  
-    let dg1 = 0 
-    let dg2 =0 
+    let d2 = 0
     let rest = 0  
     let digito 
     let nDigResult;  
     for (let nCount = 1; nCount < cpf.length -1; nCount++) {  
-          digito = parseInt(cpf.substring(nCount -1, nCount));  							
+      digito = parseInt(cpf.substring(nCount -1, nCount));  							
       d1 = d1 + ( 11 - nCount ) * digito;  
 
       d2 = d2 + ( 12 - nCount ) * digito;
     };   
     rest = (d1 % 11);  
-    dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
+    const dg1 = (rest < 2) ? 0 : 11 - rest;  
     d2 += 2 * dg1;  
     rest = (d2 % 11);  
-    if (rest < 2)  
-      dg2 = 0;  
-    else  
-      dg2 = 11 - rest; 
+    const dg2 = (rest < 2) ? 0 : 11 - rest;
       let nDigVerific = cpf.substring(cpf.length-2, cpf.length);  
     nDigResult = "" + dg1 + "" + dg2;  
     return nDigVerific == nDigResult; 
