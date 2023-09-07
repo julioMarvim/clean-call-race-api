628dbd9e2765a789b832a44ac938e18a19b2b207
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-07 11:44:27 -0300

    test: garante que cpfValidator retorne true quando o cpf valido terminar com 00

--- backend/ride/test/CpfValidator.test.ts
+++ backend/ride/test/CpfValidator.test.ts
@@ -3,3 +3,7 @@ import CpfValidator from "../src/CpfValidator"
 test("Deve validar um cpf", function () {
   expect(CpfValidator.validate("03765208094")).toBeTruthy()
 })
+
+test("Deve validar um cpf com final 00", function () {
+  expect(CpfValidator.validate("67118162000")).toBeTruthy()
+})
