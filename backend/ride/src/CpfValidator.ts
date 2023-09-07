d9977d6af2378ddd89f5a63ab08e576b3e284b8a
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-07 12:01:36 -0300

    refactor: inverte logicas de validação

--- backend/ride/src/CpfValidator.ts
+++ backend/ride/src/CpfValidator.ts
@@ -1,37 +1,32 @@
 export default class CpfValidator{
   
-  static validate (str: string) {
-      if (str.length >= 11 && str.length <= 14){
-        str=str
-          .replace('.','')
-          .replace('.','')
-          .replace('-','')
-          .replace(" ","");  
-        if (!str.split("").every(c => c === str[0])) { 
-          let     d1, d2;  
-          let     dg1, dg2, rest;  
-          let     digito;  
-            let     nDigResult;  
-          d1 = d2 = 0;  
-          dg1 = dg2 = rest = 0;  
-          for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                digito = parseInt(str.substring(nCount -1, nCount));  							
-            d1 = d1 + ( 11 - nCount ) * digito;  
-    
-            d2 = d2 + ( 12 - nCount ) * digito;
-          };   
-          rest = (d1 % 11);  
-          dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-          d2 += 2 * dg1;  
-          rest = (d2 % 11);  
-          if (rest < 2)  
-            dg2 = 0;  
-          else  
-            dg2 = 11 - rest; 
-            let nDigVerific = str.substring(str.length-2, str.length);  
-          nDigResult = "" + dg1 + "" + dg2;  
-          return nDigVerific == nDigResult; 
-        } else return false
+  static validate (cpf: string) {
+    cpf=cpf.replace('.','').replace('.','').replace('-','').replace(" ","");
+      if (cpf.length !== 11) return false
+      if (!cpf.split("").every(c => c === cpf[0])) { 
+        let     d1, d2;  
+        let     dg1, dg2, rest;  
+        let     digito;  
+          let     nDigResult;  
+        d1 = d2 = 0;  
+        dg1 = dg2 = rest = 0;  
+        for (let nCount = 1; nCount < cpf.length -1; nCount++) {  
+              digito = parseInt(cpf.substring(nCount -1, nCount));  							
+          d1 = d1 + ( 11 - nCount ) * digito;  
+  
+          d2 = d2 + ( 12 - nCount ) * digito;
+        };   
+        rest = (d1 % 11);  
+        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
+        d2 += 2 * dg1;  
+        rest = (d2 % 11);  
+        if (rest < 2)  
+          dg2 = 0;  
+        else  
+          dg2 = 11 - rest; 
+          let nDigVerific = cpf.substring(cpf.length-2, cpf.length);  
+        nDigResult = "" + dg1 + "" + dg2;  
+        return nDigVerific == nDigResult; 
 			}
 	}
 }
