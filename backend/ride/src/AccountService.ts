3365935f6bc7905bb6d210b95d5ccb68cc1b328b
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-07 11:22:34 -0300

    refactor: inverte validação do name

--- backend/ride/src/AccountService.ts
+++ backend/ride/src/AccountService.ts
@@ -80,17 +80,14 @@ export default class AccountService {
 			const date = new Date();
 			const [existingAccount] = await connection.query("select * from race.account where email = $1", [input.email]);
 			if (!existingAccount) {
-				if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-					if (! input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
-					if (!this.validateCpf(input.cpf)) throw new Error("Invalid cpf");
-					if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid plate");
-					await connection.query("insert into race.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, date, is_verified, verification_code) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, date, false, verificationCode]);
-					await this.sendEmail(input.email, "Verification", `Please verify your code at first login ${verificationCode}`);
-					return {
-						accountId
-					}
-				} else {
-					throw new Error("Invalid name");
+				if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid name");
+				if (!input.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
+				if (!this.validateCpf(input.cpf)) throw new Error("Invalid cpf");
+				if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid plate");
+				await connection.query("insert into race.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, date, is_verified, verification_code) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, date, false, verificationCode]);
+				await this.sendEmail(input.email, "Verification", `Please verify your code at first login ${verificationCode}`);
+				return {
+					accountId
 				}
 			} else {
 				throw new Error("Account already exists");
