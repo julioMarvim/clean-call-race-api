a64d0afc6b37c07e90f855d62fb20afdbcf9d746
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-08 18:33:05 -0300

    refactor: finaliza refactor cpfValidator

--- backend/ride/test/CpfValidator.test.ts
+++ backend/ride/test/CpfValidator.test.ts
@@ -1,21 +1,20 @@
 import CpfValidator from "../src/CpfValidator"
+const cpfValidator = new CpfValidator()
 
 test.each([
-  "03765208094",
-  "01234567890",
-  "565.486.780-60",
-  "671.181.620-00"
+  "95818705552",
+	"01234567890",
+	"565.486.780-60",
+	"147.864.110-00",
 ])
 ("Deve validar um cpf", function (cpf: string) {
-  expect(CpfValidator.validate(cpf)).toBeTruthy()
+  expect(cpfValidator.validate(cpf)).toBeTruthy()
 })
 
 test.each([
-  "6711816200",
-  "037652080941",
-  "00000000000",
-  ""
+  "958.187.055-00",
+	"958.187.055"
 ])
 ("Não deve validar um cpf", function (cpf: string) {
-  expect(CpfValidator.validate(cpf)).toBeFalsy()
+  expect(cpfValidator.validate(cpf)).toBeFalsy()
 })
