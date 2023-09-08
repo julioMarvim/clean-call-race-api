143a4eb27f297f4c94b741540166f4eaf8497683
julioMarvim <juliomarvim@yahoo.com.br>
2023-09-08 18:48:26 -0300

    test: melhora cobertura de testes

--- backend/ride/test/CpfValidator.test.ts
+++ backend/ride/test/CpfValidator.test.ts
@@ -2,18 +2,20 @@ import CpfValidator from "../src/CpfValidator"
 const cpfValidator = new CpfValidator()
 
 test.each([
-  "95818705552",
-	"01234567890",
-	"565.486.780-60",
-	"147.864.110-00",
+  "03765208094",
+  "01234567890",
+  "565.486.780-60",
+  "671.181.620-00"
 ])
 ("Deve validar um cpf", function (cpf: string) {
   expect(cpfValidator.validate(cpf)).toBeTruthy()
 })
 
 test.each([
-  "958.187.055-00",
-	"958.187.055"
+  "6711816200",
+  "037652080941",
+  "00000000000",
+  ""
 ])
 ("Não deve validar um cpf", function (cpf: string) {
   expect(cpfValidator.validate(cpf)).toBeFalsy()
